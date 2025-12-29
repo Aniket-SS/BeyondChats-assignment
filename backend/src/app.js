5a0c100db33ad2c3b573287f1ec79bb8e9b6398c
Aniket-SS <sonawaneaniket7@gmail.com>
2025-12-29 09:29:43 +0530

    Setup basic Express server

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+// src/app.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("BeyondChats Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

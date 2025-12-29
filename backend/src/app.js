ea68306c687b72093279bb713548aa54596acce8
Aniket-SS <sonawaneaniket7@gmail.com>
2025-12-29 13:04:08 +0530

    Add MongoDB Atlas connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,21 @@
 // src/app.js
 const express = require("express");
 const cors = require("cors");
+
 require("dotenv").config();
 
+const connectDB = require("./utils/db");
+
 const app = express();
+connectDB();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+app.get("/test-db", async (req, res) => {
+  res.send("DB connected and server running");
+});
 
 // test route
 app.get("/", (req, res) => {
